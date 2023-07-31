b54bd3a1bb067799cf8fa0666371566d4d648bbc
DrW3b <94005355+DrW3b@users.noreply.github.com>
2023-07-31 16:11:00 +0200

    Update goshooter.go

--- goshooter.go
+++ goshooter.go
@@ -20,8 +20,20 @@ var (
 	threadsFlag = flag.Int("th", 5, "number of threads")
 )
 
+const (
+	colorRed   = "\033[31m"
+	colorGreen = "\033[32m"
+	colorReset = "\033[0m"
+)
+
 func main() {
 	flag.Parse()
+
+	// Show the banner in red when the script starts.
+	printPattern(colorRed)
+
+	// Set the logger's output to os.Stdout to show logs without storing them in a file.
+	log.SetOutput(os.Stdout)
 
 	data, err := ioutil.ReadFile(*fileFlag)
 	if err != nil {
@@ -41,51 +53,59 @@ func main() {
 	var wg sync.WaitGroup
 	urlCh := make(chan string)
 
-	
 	for i := 0; i < *threadsFlag; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for url := range urlCh {
-				ctx, _ := chromedp.NewContext(context.Background()) 
+				ctx, _ := chromedp.NewContext(context.Background())
 				takeScreenshot(ctx, dir, url)
 			}
 		}()
 	}
 
-	
 	for _, url := range urls {
 		urlCh <- url
 	}
 
-	
 	close(urlCh)
 	wg.Wait()
 }
 
+func printPattern(color string) {
+	// The pattern to be printed
+	pattern := fmt.Sprintf("%s\n ██████╗  ██████╗ ███████╗██╗  ██╗ ██████╗  ██████╗ ████████╗███████╗██████╗\n", color)
+	pattern += fmt.Sprintf("██╔════╝ ██╔═══██╗██╔════╝██║  ██║██╔═══██╗██╔═══██╗╚══██╔══╝██╔════╝██╔══██╗\n")
+	pattern += fmt.Sprintf("██║  ███╗██║   ██║███████╗███████║██║   ██║██║   ██║   ██║   █████╗  ██████╔╝\n")
+	pattern += fmt.Sprintf("██║   ██║██║   ██║╚════██║██╔══██║██║   ██║██║   ██║   ██║   ██╔══╝  ██╔══██╗\n")
+	pattern += fmt.Sprintf("╚██████╔╝╚██████╔╝███████║██║  ██║╚██████╔╝╚██████╔╝   ██║   ███████╗██║  ██║\n")
+	pattern += fmt.Sprintf(" ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝%s\n", colorReset)
+	pattern += fmt.Sprintf("%s%-17s%s\n", colorGreen, "                          goshooter v1.0", colorReset)
+	pattern += fmt.Sprintf("%s%-13s%s\n", colorRed, "                          Coded by DrW3B", colorReset)
+	pattern += fmt.Sprintf("%s                          Telegram: @DrW33B_Xo%s\n", colorRed, colorReset)
+
+	fmt.Println(pattern)
+}
+
+
+
+
 func takeScreenshot(ctx context.Context, dir, url string) {
-	
 	if url == "" {
 		return
 	}
 
-	fmt.Printf("Taking screenshot of %s\n", url)
-
-	
 	fullPath := filepath.Join(dir, url, "screenshot.png")
 
-	
 	err := os.MkdirAll(filepath.Dir(fullPath), 0755)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to create directory: %v", err)
 	}
 
-	
 	if strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://") {
-		log.Fatalf("Invalid URL: %s. Please provide a domain without protocol.", url)
+		log.Fatalf("%sInvalid URL: %s. Please provide a domain without protocol.%s\n", colorRed, url, colorReset)
 	}
 
-	
 	var buf []byte
 	err = chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.EmulateViewport(1920, 1080),
@@ -94,13 +114,12 @@ func takeScreenshot(ctx context.Context, dir, url string) {
 		chromedp.CaptureScreenshot(&buf),
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%sFailed to capture screenshot: %v%s\n", colorRed, err, colorReset)
 	}
 
 	if err := ioutil.WriteFile(fullPath, buf, 0644); err != nil {
-		log.Fatal(err)
+		log.Fatalf("%sFailed to write screenshot to file: %v%s\n", colorRed, err, colorReset)
 	}
 
-	fmt.Printf("Screenshot of %s saved\n", url)
+	log.Printf("%sScreenshot of %s saved%s\n", colorGreen, url, colorReset)
 }
-
